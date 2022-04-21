dc6bf8f4226d44042f6238e9052d3827d6cf6bb4
dominikusvieri <dominikusvieri9@gmail.com>
2022-04-21 17:48:23 +0700

    Create Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header ></Header>
+      <main>
+        <h1>Main</h1>
+      </main>
+      <Footer ></Footer>
+    </>
   );
 }
 
