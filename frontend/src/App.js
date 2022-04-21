77757c40792b0a59e83f5c567e59e48f21ee4dbd
dominikusvieri <dominikusvieri9@gmail.com>
2022-04-21 21:07:42 +0700

    Kurang Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import { Container } from 'react-bootstrap';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header ></Header>
       <main>
         <Container>
-          <h1>Welcome</h1>
+          <Route exact path='/' component={HomePage}></Route>   
+          <Route path='/product/:id' component={ProductPage}></Route>    
         </Container>
       </main>
       <Footer ></Footer>
-    </>
+    </Router>
   );
 }
 
